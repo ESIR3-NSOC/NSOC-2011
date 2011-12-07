da6a0d2c96c800a687aca7e5caf3e946874d3a71
jackybourgeois <jacky.bourgeois@gmail.com>
2011-12-07 09:25:10 +0100

    Fonctionnalités de l'application

---
+++ docs/Fonctionnalit� de l'application.mm
@@ -0,0 +1,55 @@
+<map version="0.8.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1323243781156" ID="Freemind_Link_1009117782" MODIFIED="1323243811607" TEXT="Fonctionnalit&#xe9; de l&apos;application ">
+<node CREATED="1323243842501" ID="Freemind_Link_1150733497" MODIFIED="1323243863656" POSITION="right" TEXT="Sorties">
+<node CREATED="1323243865307" ID="Freemind_Link_1449560092" MODIFIED="1323243897622" TEXT="Commander les volets"/>
+<node CREATED="1323244148288" ID="Freemind_Link_825887818" MODIFIED="1323244151299" TEXT="IHM">
+<node CREATED="1323243898946" ID="Freemind_Link_91187285" MODIFIED="1323243908833" TEXT="Produire des rapports d&apos;&#xe9;tat"/>
+<node CREATED="1323244157587" ID="Freemind_Link_1209178764" MODIFIED="1323244544686" TEXT="Ecran"/>
+</node>
+<node CREATED="1323243910500" ID="Freemind_Link_205447411" MODIFIED="1323243953004" TEXT="Ballast lumi&#xe8;res"/>
+<node CREATED="1323243961388" ID="Freemind_Link_597719788" MODIFIED="1323243966127" TEXT="Chauffages"/>
+<node CREATED="1323243970189" ID="Freemind_Link_1777916382" MODIFIED="1323244037333" TEXT="Vid&#xe9;oprojecteur + &#xe9;cran"/>
+<node CREATED="1323244038102" ID="Freemind_Link_1800213746" MODIFIED="1323244076178" TEXT="Serrures &#xe9;lectromagn&#xe9;tique"/>
+<node CREATED="1323244095254" ID="_" MODIFIED="1323244099243" TEXT="Moteur cam&#xe9;ra"/>
+<node CREATED="1323245129604" ID="Freemind_Link_1583601562" MODIFIED="1323245176383" TEXT="Prdouction locale d&apos;&#xe9;nergie"/>
+<node CREATED="1323245176984" ID="Freemind_Link_694169489" MODIFIED="1323245188434" TEXT="Choisir son fournisseur d&apos;energie"/>
+</node>
+<node CREATED="1323244651352" ID="Freemind_Link_851127365" MODIFIED="1323244655257" POSITION="left" TEXT="Entr&#xe9;es">
+<node CREATED="1323244276620" ID="Freemind_Link_408695023" MODIFIED="1323244411498" TEXT="Interface de commande multimodale">
+<node CREATED="1323244413378" ID="Freemind_Link_1426149604" MODIFIED="1323244446133" TEXT="Commande vocale"/>
+<node CREATED="1323244481700" ID="Freemind_Link_1984450719" MODIFIED="1323244489785" TEXT="Interface touchscreen"/>
+<node CREATED="1323244490940" ID="Freemind_Link_380470368" MODIFIED="1323244501460" TEXT="Interrupteurs"/>
+<node CREATED="1323244502049" ID="Freemind_Link_878173671" MODIFIED="1323244569646" TEXT="Kynect commande par mouvement"/>
+<node CREATED="1323244548388" ID="Freemind_Link_176499704" MODIFIED="1323244599008" TEXT="WiiMote commande mouvement avec &#xe9;quippements"/>
+</node>
+<node CREATED="1323244175324" ID="Freemind_Link_1355932414" MODIFIED="1323244222234" TEXT="Capteurs">
+<node CREATED="1323244224267" ID="Freemind_Link_1496501286" MODIFIED="1323244228402" TEXT="Luminosit&#xe9;"/>
+<node CREATED="1323244229058" ID="Freemind_Link_452078176" MODIFIED="1323244230733" TEXT="Pr&#xe9;sence"/>
+<node CREATED="1323244231290" ID="Freemind_Link_1503298664" MODIFIED="1323244233070" TEXT="Cam&#xe9;ra"/>
+<node CREATED="1323244233431" ID="Freemind_Link_1446926159" MODIFIED="1323244236674" TEXT="Station m&#xe9;t&#xe9;o"/>
+<node CREATED="1323244245660" ID="Freemind_Link_456479959" MODIFIED="1323244250897" TEXT="Humidit&#xe9;"/>
+<node CREATED="1323244251422" ID="Freemind_Link_1775780029" MODIFIED="1323244253376" TEXT="Temp&#xe9;rature"/>
+</node>
+<node CREATED="1323244338872" ID="Freemind_Link_29931966" MODIFIED="1323244348216" TEXT="Service disponible sur le web">
+<node CREATED="1323244329983" ID="Freemind_Link_517682729" MODIFIED="1323244336353" TEXT="Pr&#xe9;vision m&#xe9;t&#xe9;o"/>
+<node CREATED="1323244210937" ID="Freemind_Link_1246848029" MODIFIED="1323244387246" TEXT="Plusieurs Agenda"/>
+<node CREATED="1323244623113" ID="Freemind_Link_1810450222" MODIFIED="1323244807271" TEXT="Prix de l&apos;&#xe9;nergie par fournisseur"/>
+<node CREATED="1323244746976" ID="Freemind_Link_1380814032" MODIFIED="1323244746976" TEXT=""/>
+</node>
+</node>
+<node CREATED="1323245238385" ID="Freemind_Link_166758384" MODIFIED="1323245253237" POSITION="right" TEXT="Fonctionnalit&#xe9;s">
+<node CREATED="1323245265088" ID="Freemind_Link_528151587" MODIFIED="1323245369393" TEXT="Gestion de l&apos;&#xe9;nergie par rapport &#xe0; l&apos;agenda"/>
+<node CREATED="1323245369931" ID="Freemind_Link_1177648812" MODIFIED="1323245391504" TEXT="Gestion de la s&#xe9;curit&#xe9; par rapport &#xe0; l&apos;agenda"/>
+<node CREATED="1323245392785" ID="Freemind_Link_1636143880" MODIFIED="1323245460625" TEXT="Etalissement du rapport de (production - consommation d&apos;&#xe9;nergie) "/>
+<node CREATED="1323245482059" ID="Freemind_Link_1421413501" MODIFIED="1323245510316" TEXT="Affichage de l&apos;&#xe9;tat en temps r&#xe9;el sur une interface">
+<node CREATED="1323245513172" ID="Freemind_Link_315875105" MODIFIED="1323245515507" TEXT="Remote"/>
+<node CREATED="1323245515841" ID="Freemind_Link_31993650" MODIFIED="1323245517785" TEXT="Local"/>
+</node>
+<node CREATED="1323245640150" ID="Freemind_Link_315866858" MODIFIED="1323245652250" TEXT="Gestion de l&apos;&#xe9;nergie par rapport aux capteurs"/>
+<node CREATED="1323245661049" ID="Freemind_Link_1738525086" MODIFIED="1323245727596" TEXT="Moteur de suggestion d&apos;action pour l&apos;&#xe9;conomie d&apos;&#xe9;nergie"/>
+<node CREATED="1323245728215" ID="Freemind_Link_1826076746" MODIFIED="1323245738335" TEXT="Fonctionnement semi-automatique"/>
+<node CREATED="1323245739145" ID="Freemind_Link_1374029076" MODIFIED="1323245751051" TEXT="Fonctionnement full automatique"/>
+</node>
+</node>
+</map>
