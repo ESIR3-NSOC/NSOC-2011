f72963a4647ffdd570642eb800240d47507b812f
jackybourgeois <jacky.bourgeois@gmail.com>
2011-12-26 21:39:24 +0100

    update database file

--- db/database.sql
+++ db/database.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client: 127.0.0.1
--- Généré le : Mer 21 Décembre 2011 à 22:26
+-- Généré le : Lun 26 Décembre 2011 à 21:38
 -- Version du serveur: 5.5.16
 -- Version de PHP: 5.3.8
 
@@ -19,6 +19,29 @@ SET time_zone = "+00:00";
 --
 -- Base de données: `nsoc11`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `tasks`
+--
+
+CREATE TABLE IF NOT EXISTS `tasks` (
+  `id` varchar(36) COLLATE utf8_bin NOT NULL,
+  `description` text COLLATE utf8_bin NOT NULL,
+  `date` datetime NOT NULL,
+  `taskstate` varchar(20) COLLATE utf8_bin NOT NULL,
+  `script` text COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Contenu de la table `tasks`
+--
+
+INSERT INTO `tasks` (`id`, `description`, `date`, `taskstate`, `script`) VALUES
+('b33d20bd-2faf-11e1-81b8-0021cc4198bb', 'un deuxième test', '2011-12-26 04:29:16', 'WAITING', 'le script !!'),
+('e1f4f0a9-2d56-11e1-8e5b-0021cc4198bb', 'essai task update', '2011-12-23 01:23:05', 'WAITING', 'le script !! ...');
 
 -- --------------------------------------------------------
 
