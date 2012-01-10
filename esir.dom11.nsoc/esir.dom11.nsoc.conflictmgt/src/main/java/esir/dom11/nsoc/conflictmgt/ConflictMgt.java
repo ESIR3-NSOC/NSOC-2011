aa14f43eabfd790c058daeed4a35200dea93bb33
jackybourgeois <jacky.bourgeois@gmail.com>
2012-01-10 13:44:19 +0100

    update conflct mgt

--- esir.dom11.nsoc/esir.dom11.nsoc.conflictmgt/src/main/java/esir/dom11/nsoc/conflictmgt/ConflictMgt.java
+++ esir.dom11.nsoc/esir.dom11.nsoc.conflictmgt/src/main/java/esir/dom11/nsoc/conflictmgt/ConflictMgt.java
@@ -1,10 +1,30 @@
 package esir.dom11.nsoc.conflictmgt;
 
+/*
+ TODO: thread gestion temps lock
+    save list cmd et action dans bd (30min)
+     gérer condition actuator free
+     gérer catégorie
+  */
+
+import esir.dom11.nsoc.model.Action;
+import esir.dom11.nsoc.model.Command;
 import org.kevoree.annotation.*;
 import org.kevoree.framework.AbstractComponentType;
+import org.kevoree.framework.MessagePort;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.UUID;
+
+@Provides({
+    @ProvidedPort(name = "cmdFromCtrl", type = PortType.MESSAGE)
+})
+@Requires({
+    @RequiredPort(name = "actToActuator", type = PortType.MESSAGE)
+})
 @Library(name = "NSOC_2011")
 @ComponentType
 public class ConflictMgt extends AbstractComponentType {
@@ -19,6 +39,8 @@ public class ConflictMgt extends AbstractComponentType {
     * Attributes
     */
 
+    private LinkedList<Command> _commandBufferList;
+    private HashMap<UUID,Action> _lastActuatorActionMap;
 
     /*
      * Getters / Setters
@@ -31,6 +53,8 @@ public class ConflictMgt extends AbstractComponentType {
     @Start
     public void start() {
         logger.info("= = = = = start conflict manager = = = = = =");
+        _lastActuatorActionMap = new HashMap<UUID, Action>();
+        _commandBufferList = new LinkedList<Command>();
     }
 
     @Stop
@@ -41,5 +65,39 @@ public class ConflictMgt extends AbstractComponentType {
     @Update
     public void update() {
         logger.info("= = = = = update conflict manager = = = = = =");
+    }
+    
+    /*
+     * Overrides
+     */
+
+    @Port(name = "cmdFromCtrl")
+    public void cmdFromCtrl(Command command) {
+        _commandBufferList.add(command);
+        for (Action action : command.getActionList()) {
+            if (isActuatorFree(action)) {
+                _lastActuatorActionMap.put(action.getIdActuator(),action);
+                getPortByName("actToActuator",MessagePort.class).process(action);
+            }
+        }
+
+    }
+
+    /*
+     * Methods
+     */
+
+    private boolean isActuatorFree(Action action) {
+        // TODO
+        return true;
+    }
+
+    private void saveInDb() {
+        // TODO
+    }
+    
+    private int getPriority() {
+        // TODO
+        return 0;
     }
 }
