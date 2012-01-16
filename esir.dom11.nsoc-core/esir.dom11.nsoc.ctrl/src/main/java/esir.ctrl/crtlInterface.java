45bca1860c9d149a5761764563203361db149c80
aesnaux <anthony.esnaux@gmail.com>
2012-01-16 14:46:46 +0100

    Interface

---
+++ esir.dom11.nsoc-core/esir.dom11.nsoc.ctrl/src/main/java/esir.ctrl/crtlInterface.java
@@ -0,0 +1,20 @@
+package esir.ctrl;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Anthony
+ * Date: 16/01/12
+ * Time: 14:42
+ * To change this template use File | Settings | File Templates.
+ */
+public interface crtlInterface {
+    void send2IHM();
+    void send2DAO();
+    void send2Conflict();
+    void getContext();
+    void send2Context();
+    void receiveIHM();
+    void receiveConflict();
+
+
+}
