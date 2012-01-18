ce1ad4b3798519c9317c8200d26af5d45d1d09d4
cacamille3 <guerincamille56@gmail.com>
2012-01-18 09:23:53 +0100

    Passage en UTF-8

--- esir.dom11.nsoc-core/esir.dom11.nsoc.ctrl/src/main/java/esir/dom11/nsoc/ctrl/crtlInterface.java
+++ esir.dom11.nsoc-core/esir.dom11.nsoc.ctrl/src/main/java/esir/dom11/nsoc/ctrl/crtlInterface.java
@@ -9,27 +9,26 @@ package esir.dom11.nsoc.ctrl;
  */
 public interface crtlInterface {
     // Ctrl qui envoie vers l'IHM
-	//Send2... vont permettre d'envoyer les données vers les différents composants
-	
+	//Send2... vont permettre d'envoyer les donn√©es vers les diff√©rents composants
 	//IHM besoin de data + ...
     void send2IHM(Data data);
     
-    //Envoie de tout ce qui a pu être modifié.
+    //Envoie de tout ce qui a pu √™tre modifi√©.
     void send2DAO(Data data );
     void send2DAO(Command command);
     
     //Envoie d'une liste d'actions (= command) et Conflict s'occupe de voir si il n'y a pas de conflict
     void send2Conflict(Command command);
     
-    // Demande d'une variable auprès du contexte (ex : capteurs temperature chambre )
+    // Demande d'une variable aupr√®s du contexte (ex : capteurs temperature chambre )
     Data send2Context(DataType datatype);
     
-    //Receive... vont permettre de recevoir les données des différents composants
+    //Receive... vont permettre de recevoir les donn√©es des diff√©rents composants
     //De type listeners (automatiquement)
     
-    //On reçoit une requête de la part des IHM pour avoir des données
+    //On re√ßoit une requ√™te de la part des IHM pour avoir des donn√©es
     void receiveIHM();
-    //On reçoit une requête de la part du conflict manager pour d'autres données.
+    //On re√ßoit une requ√™te de la part du conflict manager pour d'autres donn√©es.
     void receiveConflict();
     
     //getContext = demande de notre part du contexte
