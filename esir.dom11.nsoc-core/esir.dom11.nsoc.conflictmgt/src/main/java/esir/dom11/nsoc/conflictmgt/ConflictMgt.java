26b72ce6061815c1117b93f1795bdaa773262c71
Pierre <pierre@pierre-XPS-M1530.(none)>
2012-01-23 13:17:56 +0100

    retrieve update which was lost by forced-pushed this weekend... please be careful!

--- esir.dom11.nsoc-core/esir.dom11.nsoc.conflictmgt/src/main/java/esir/dom11/nsoc/conflictmgt/ConflictMgt.java
+++ esir.dom11.nsoc-core/esir.dom11.nsoc.conflictmgt/src/main/java/esir/dom11/nsoc/conflictmgt/ConflictMgt.java
@@ -12,15 +12,13 @@ import org.kevoree.framework.MessagePort;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.UUID;
+import java.util.*;
 
 @Provides({
-    @ProvidedPort(name = "cmdFromCtrl", type = PortType.MESSAGE)
+        @ProvidedPort(name = "cmdFromCtrl", type = PortType.MESSAGE)
 })
 @Requires({
-    @RequiredPort(name = "actToActuator", type = PortType.MESSAGE)
+        @RequiredPort(name = "actToActuator", type = PortType.MESSAGE)
 })
 @Library(name = "NSOC_2011")
 @ComponentType
@@ -35,18 +33,28 @@ public class ConflictMgt extends AbstractComponentType {
     /*
     * Attributes
     */
+    private long UpdateDelay = 60000;                   //time between updates of locks, in ms (60s by befault)
+    private static int SECURITY = 0;
+    private static int USER = 1;
+    private static int AUTO = 2;
 
-    private LinkedList<Command> _commandBufferList;         //buffer des dernières commandes reçues avant traitement et sauvegarde
-    private HashMap<UUID,Action> _lastActuatorActionMap;    //list des actions acceptées et envoyées pour gestion des conflits
+    private LinkedList<Command> _commandBufferList;         // buffer of the last received command, before process and save
+    private LinkedList<Command> _commandWithTimeout;        // buffer of the last received command, before process and save
+    private HashMap<UUID,Action> _lastActuatorActionMap;    // list of accepted and send actions, for conflict management
+    private HashMap<UUID,Long> _lockActuatorMap;             // list of locks on the actuator, updated all the 60s by default
+    private Timer timer;
 
     /*
-     * Getters / Setters
-     */
+    * Getters / Setters
+    */
 
     /*
     * Overrides
     */
 
+    /**
+     *
+     */
     @Start
     public void start() {
         logger.info("= = = = = start conflict manager = = = = = =");
@@ -54,6 +62,47 @@ public class ConflictMgt extends AbstractComponentType {
         //Initialisation
         _lastActuatorActionMap = new HashMap<UUID, Action>();
         _commandBufferList = new LinkedList<Command>();
+        _commandWithTimeout = new LinkedList<Command>();
+        _lockActuatorMap = new HashMap<UUID, Long>();
+
+        //Timer initialisation
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+
+                // Manage the timeout options, and update it
+                for(Command cmd: _commandWithTimeout){
+                    boolean[] freedom = new boolean[cmd.getActionList().size()];
+
+                    //is all the actuators command now free?
+                    for (Action action : cmd.getActionList()){
+                        if(isActuatorFree(action)){
+
+                        }
+                    }
+
+                    //update the timeout if not free
+                    int index = _commandWithTimeout.indexOf(cmd);
+                    if (cmd.getTimeOut()!=0){
+                        cmd.setTimeOut(cmd.getTimeOut()- UpdateDelay);
+                        _commandWithTimeout.remove(index);
+                        _commandWithTimeout.push(cmd);
+                    }
+                    else {
+                        _commandWithTimeout.remove(index);
+                    }
+                }
+
+                // Manage the lock option 
+                for(Map.Entry<UUID,Long> actMap: _lockActuatorMap.entrySet()){
+                    if (actMap.getValue()!=0){
+                        _lockActuatorMap.put(actMap.getKey(),actMap.getValue()- UpdateDelay);
+                    }
+                    else {
+                        _lockActuatorMap.remove(actMap.getKey());}
+                }
+            }
+        }, UpdateDelay);
     }
 
     @Stop
@@ -65,23 +114,41 @@ public class ConflictMgt extends AbstractComponentType {
     public void update() {
         logger.info("= = = = = update conflict manager = = = = = =");
     }
-    
-    /*
-     * Overrides
-     */
 
+    /*
+    * Overrides
+    */
+
+    /**
+     * cmdFromCtrl
+     * @param command
+     */
     @Port(name = "cmdFromCtrl")
     public void cmdFromCtrl(Command command) {
 
-        //Sauvegarde de la commande à traiter
+        boolean commandToSave = false;
+
+        // Save of the command to process
         _commandBufferList.add(command);
 
-        //v1, autorise les actions qui agissent sur un actionneur non présent dans _lastActuatorActionMap
+        // Retrieve lock times and send authored actions
         for (Action action : command.getActionList()) {
+
+            _lockActuatorMap.put(action.getIdActuator(), command.getLock());
+
             if (isActuatorFree(action)) {
-                _lastActuatorActionMap.put(action.getIdActuator(),action);
-                getPortByName("actToActuator",MessagePort.class).process(action);
+                send2Actuator(action);
             }
+
+            // if one action can't be done, don't check the others of the command
+            else {
+                commandToSave = true;
+                break;}
+        }
+
+        // if the command can't be done and if the timeout isn't zero, save the command in _commandWithTimeout
+        if (commandToSave && command.getTimeOut()!=0){
+            _commandWithTimeout.add(command);
         }
 
     }
@@ -90,10 +157,15 @@ public class ConflictMgt extends AbstractComponentType {
      * Methods
      */
 
+    private void send2Actuator(Action action){
+        _lastActuatorActionMap.put(action.getIdActuator(),action);
+        getPortByName("actToActuator",MessagePort.class).process(action);
+    }
+
     /**
      * isActuatorFree
      * @param action
-     * @return "true" si l'IdActuator de l'action n'est pas dans le _lastActuatorActionMap
+     * @return "true" if the IdActuator of the action isn't in the _lastActuatorActionMap
      */
     private boolean isActuatorFree(Action action) {
         if (_lastActuatorActionMap.containsKey(action.getIdActuator())){
@@ -107,7 +179,7 @@ public class ConflictMgt extends AbstractComponentType {
     private void saveInDb() {
         // TODO
     }
-    
+
     private int getPriority() {
         // TODO
         return 0;
