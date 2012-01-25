cd40d1caef3dbe3634cdaf1ade1d52d1acb7168e
Pierre <pierre@pierre-XPS-M1530.(none)>
2012-01-25 09:03:13 +0100

    update Log model

--- esir.dom11.nsoc-core/esir.dom11.nsoc.model/src/main/java/esir/dom11/nsoc/model/Log.java
+++ esir.dom11.nsoc-core/esir.dom11.nsoc.model/src/main/java/esir/dom11/nsoc/model/Log.java
@@ -10,6 +10,7 @@ public class Log {
      */
     
     private final UUID _id;
+    private final String _from;
     private final String _message;
     private final Date _date;
     private final LogLevel _logLevel;
@@ -21,20 +22,23 @@ public class Log {
     public Log() {
         _id = UUID.fromString("00000000-0000-0000-0000-000000000000");
         _date = new Date();
+        _from = "";
         _message = "";
         _logLevel = LogLevel.INFO;
     }
 
-    public Log(String message, LogLevel logLevel) {
+    public Log(String from, String message, LogLevel logLevel) {
         _id = UUID.randomUUID();
+        _from = from;
         _date = new Date();
         _message = message;
         _logLevel = logLevel;
     }
 
-    public Log(UUID id, Date date, String message, LogLevel logLevel) {
+    public Log(UUID id, Date date, String from, String message, LogLevel logLevel) {
         _id = id;
         _date = date;
+        _from = from;
         _message = message;
         _logLevel = logLevel;
     }
@@ -49,6 +53,10 @@ public class Log {
 
     public String getMessage() {
         return _message;
+    }
+
+    public String getFrom() {
+        return _from;
     }
 
     public Date getDate() {
@@ -68,6 +76,7 @@ public class Log {
         return "\n* * * Log " + getId() + " * * *"
                 + "\nLog Lavel: " + getLogLevel().getValue()
                 + "\nDate: " + getDate()
+                + "\nFrom:" +getFrom()
                 + "\nMessage: " + getMessage() + "\n";
     }
 }
